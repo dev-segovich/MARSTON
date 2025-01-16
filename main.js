945867f9f33f527599909fcb0eae735db8e3e3df
chixchix55 <jsegovich55@gmail.com>
2025-01-16 14:56:49 -0400

    New Main page  16/1/2025

--- main.js
+++ main.js
@@ -29,3 +29,10 @@
     }
     
 
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $('#navbar').addClass('scrolled');
+        } else {
+            $('#navbar').removeClass('scrolled');
+        }
+    });
