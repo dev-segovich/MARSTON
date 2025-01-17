cec3c8b0da9d2c21638fc6cc0312fe6cda801ccb
chixchix55 <jsegovich55@gmail.com>
2025-01-17 16:42:51 -0400

    Main Page  17/1/2025

--- main.js
+++ main.js
@@ -36,3 +36,70 @@
             $('#navbar').removeClass('scrolled');
         }
     });
+
+    const projectContainer = document.querySelector('.project-container');
+
+    const proyecto1Link = document.querySelector('.proyecto1-link');
+    const proyecto2Link = document.querySelector('.proyecto2-link');
+    const proyecto3Link = document.querySelector('.proyecto3-link');
+    
+    const proyecto1Imagen = document.querySelector('.proyecto1');
+    const proyecto2Imagen = document.querySelector('.proyecto2');
+    const proyecto3Imagen = document.querySelector('.proyecto3');
+    
+    const proyecto1Columna = document.querySelector('.proyecto1-link .project-image');
+    const proyecto2Columna = document.querySelector('.proyecto2-link .project-image');
+    const proyecto3Columna = document.querySelector('.proyecto3-link .project-image');
+
+    const proyecto1Info = document.querySelector('.proyecto1-info');
+    const proyecto2Info = document.querySelector('.proyecto2-info');
+    const proyecto3Info = document.querySelector('.proyecto3-info');
+
+    projectContainer.addEventListener('mouseout', () => {
+        proyecto1Imagen.style.opacity = 0;
+        proyecto2Imagen.style.opacity = 0;
+        proyecto3Imagen.style.opacity = 0;
+        proyecto1Columna.style.opacity = 1;
+        proyecto2Columna.style.opacity = 1;
+        proyecto3Columna.style.opacity = 1;
+        proyecto1Info.style.opacity = 1;
+        proyecto2Info.style.opacity = 1;
+        proyecto3Info.style.opacity = 1;
+    });
+
+    proyecto1Link.addEventListener('mouseover', () => {
+      proyecto1Imagen.style.opacity = 1;
+      proyecto2Imagen.style.opacity = 0;
+      proyecto3Imagen.style.opacity = 0;
+      proyecto1Columna.style.opacity = 0;
+      proyecto2Columna.style.opacity = 0;
+      proyecto3Columna.style.opacity = 0;
+      proyecto1Info.style.opacity = 1;
+      proyecto2Info.style.opacity = 0.1;
+      proyecto3Info.style.opacity = 0.1;
+    });
+    
+    proyecto2Link.addEventListener('mouseover', () => {
+      proyecto1Imagen.style.opacity = 0;
+      proyecto2Imagen.style.opacity = 1;
+      proyecto3Imagen.style.opacity = 0;
+      proyecto1Columna.style.opacity = 0;
+      proyecto2Columna.style.opacity = 0;
+      proyecto3Columna.style.opacity = 0;
+      proyecto1Info.style.opacity = 0.1;
+      proyecto2Info.style.opacity = 1;
+      proyecto3Info.style.opacity = 0.1;
+    });
+    
+    proyecto3Link.addEventListener('mouseover', () => {
+      proyecto1Imagen.style.opacity = 0;
+      proyecto2Imagen.style.opacity = 0;
+      proyecto3Imagen.style.opacity = 1;
+      proyecto1Columna.style.opacity = 0;
+      proyecto2Columna.style.opacity = 0;
+      proyecto3Columna.style.opacity = 0;
+      proyecto1Info.style.opacity = 0.1;
+      proyecto2Info.style.opacity = 0.1;
+      proyecto3Info.style.opacity = 1;
+    });
+
